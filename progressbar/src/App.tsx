9dbfd7fbaaec2493a2e228ed8a7c21fc7d952cf3
Thamaraiselvi2109 <thamaraiselvig2109@gmail.com>
2025-03-21 10:34:10 +0530

    progress bar

--- progressbar/src/App.tsx
+++ progressbar/src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   const bars = [5, 10, 20, 29, 50, 75, 100]
   
   return (
-    <>
+    <div className='app'>
       <h1>Progress Bar</h1>
       {bars.map((item) => <ProgressBar key={item} progress={item} />)}
-    </>
+    </div>
   )
 }
 
